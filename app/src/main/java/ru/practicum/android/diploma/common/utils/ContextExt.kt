eded1001d4bcfded554c6d54f4338a365d20aa7b
Aleksandr Poliakov <master@artwist.ru>
2023-12-02 19:11:27 +0300

    Создаёт расширение для проверки статуса интернета

---
+++ app/src/main/java/ru/practicum/android/diploma/common/utils/ContextExt.kt
@@ -0,0 +1,17 @@
+package ru.practicum.android.diploma.common.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+fun Context.checkInternetReachability(): Boolean {
+    val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val network = connectivityManager.activeNetwork ?: return false
+    val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+    return when {
+        activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+        activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+        else -> false
+    }
+}
+
