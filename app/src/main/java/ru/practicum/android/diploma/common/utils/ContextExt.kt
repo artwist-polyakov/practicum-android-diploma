f6a2d0a620a85000e00585d20435bbd6bbb289a7
Aleksandr Poliakov <master@artwist.ru>
2023-12-02 19:20:00 +0300

    добавляет документацию для расширения

--- app/src/main/java/ru/practicum/android/diploma/common/utils/ContextExt.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/utils/ContextExt.kt
@@ -4,6 +4,20 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
+/**
+ * Проверяет доступность интернета, возвращает:
+ * - true — если интернет доступен,
+ * - false — если интернет недоступен.
+ *
+ * Пример вызова: requireContext().checkInternetReachability()
+ *
+ *  Эта функция работает следующим образом:
+ *  1. Получает сервис системы CONNECTIVITY_SERVICE, который предоставляет информацию о состоянии сети.
+ *  2. Проверяет, есть ли активная сеть. Если нет, возвращает false.
+ *  3. Получает возможности активной сети. Если их нет, возвращает false.
+ *  4. Проверяет, доступен ли интернет через Wi-Fi или мобильную сеть. Если да, возвращает true.
+ *  5. Если интернет не доступен ни через Wi-Fi, ни через мобильную сеть, возвращает false.
+ */
 fun Context.checkInternetReachability(): Boolean {
     val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val network = connectivityManager.activeNetwork ?: return false
