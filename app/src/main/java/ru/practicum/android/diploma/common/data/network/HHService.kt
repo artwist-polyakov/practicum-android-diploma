37b9480f9a85cb4a216bb2238a4cd87aeffcc0eb
Aleksandr Poliakov <master@artwist.ru>
2023-12-03 14:06:52 +0300

    добавляет интерфейс для запроса вакансий

--- app/src/main/java/ru/practicum/android/diploma/common/data/network/HHService.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/data/network/HHService.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.common.data.network
 
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 import ru.practicum.android.diploma.common.data.dto.HHSearchResponce
 
@@ -8,8 +9,13 @@ interface HHService {
 
     @GET("vacancies")
     suspend fun searchVacancies(
+        @Header("Authorization") token: String,
         @Query("text") text: String = "",
         @Query("page") page: Int = 0,
         @Query("per_page") perPage: Int = 20,
+        @Query("area") area: Int? = null,
+        @Query("industry") industry: Int? = null,
+        @Query("salary") salary: Int? = null,
+        @Query("only_with_salary") onlyWithSalary: Boolean = false
     ): HHSearchResponce
 }
