423fe5287d9ffa36fafb0f537ac06b6f5750ab10
NikolayRyabcev <nikolas14@mail.ru>
2023-12-03 10:56:18 +0300

    1. Добавлена 1 версия ДАО

---
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/FavoutiteVacancyDAO.kt
@@ -0,0 +1,23 @@
+package ru.practicum.android.diploma.data.db.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+/*
+@Dao
+interface FavoutiteVacancyDAO {
+    @Insert(entity = VacancyEntity::class, onConflict = OnConflictStrategy.REPLACE)
+    fun insertTrack (vacancy: VacancyEntity)
+
+    @Delete(entity = VacancyEntity::class)
+    fun deleteTrack (track:VacancyEntity)
+
+    @Query("SELECT * FROM favourite_vacancy_table ORDER BY addTime DESC")
+    fun queryTrack():List<VacancyEntity>
+
+    @Query("SELECT * FROM favourite_vacancy_table WHERE trackId=:vacancyId")
+    fun queryTrackId(searchId:Long):VacancyEntity
+}
+*/
