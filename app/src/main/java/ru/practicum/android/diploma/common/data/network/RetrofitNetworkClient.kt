475f78581c4cc3581a9e1f6275acde63ec0cf4da
Aleksandr Poliakov <master@artwist.ru>
2023-12-03 14:11:13 +0300

    добавляет апи в конструктор ретрофита

--- app/src/main/java/ru/practicum/android/diploma/common/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/data/network/RetrofitNetworkClient.kt
@@ -3,8 +3,10 @@ package ru.practicum.android.diploma.common.data.network
 import android.content.Context
 import ru.practicum.android.diploma.common.data.dto.Response
 
-class RetrofitNetworkClient(private val hhService: HHService,
-    private val context: Context) : NetworkClient {
+class RetrofitNetworkClient(
+    private val hhService: HHService,
+    private val context: Context
+) : NetworkClient {
     override suspend fun doRequest(dto: Any): Response {
         TODO("Not yet implemented")
     }
