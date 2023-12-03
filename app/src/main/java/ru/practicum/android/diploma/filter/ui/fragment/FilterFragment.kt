8352e6d54c828d48c395f7e24ca05562e4f8b7ea
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-12-03 16:41:32 +0300

    Добавляет зависимость в FilterFragment

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/fragment/FilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/fragment/FilterFragment.kt
@@ -1,10 +1,12 @@
 package ru.practicum.android.diploma.filter.ui.fragment
 
 import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import ru.practicum.android.diploma.common.ui.BaseFragment
 import ru.practicum.android.diploma.databinding.FragmentFilterBinding
 import ru.practicum.android.diploma.filter.ui.viewmodel.FilterViewModel
 
+@AndroidEntryPoint
 class FilterFragment : BaseFragment<FragmentFilterBinding, FilterViewModel>(FragmentFilterBinding::inflate) {
     override val viewModel by viewModels<FilterViewModel>()
 
