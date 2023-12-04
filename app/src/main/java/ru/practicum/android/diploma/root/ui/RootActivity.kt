b1bb84237ba0659029f5204ff20ef428f66671e2
Aleksandr Poliakov <master@artwist.ru>
2023-12-04 15:37:28 +0300

    правит замечания детект

--- app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -7,9 +7,9 @@ import android.view.View
 import android.view.WindowInsetsController
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
-import dagger.hilt.android.AndroidEntryPoint
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.common.ui.BaseActivity
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
@@ -25,9 +25,9 @@ class RootActivity : BaseActivity<ActivityRootBinding>(ActivityRootBinding::infl
         setStatusBarColor()
     }
 
-    private fun networkRequestExample(accessToken: String) {
-        // ...
-    }
+//    private fun networkRequestExample(accessToken: String) {
+//        // ...
+//    }
 
     /**
      *Метод управляет видимостью bottomNavigationView
