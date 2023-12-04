e5c6f54c1445352faecf8dd6c3a498da876cdbbb
Aleksandr Poliakov <master@artwist.ru>
2023-12-04 22:25:59 +0300

    Реализовывет репозиторий для получения данных по сети

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/HHSearchRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/HHSearchRepository.kt
@@ -5,10 +5,18 @@ import ru.practicum.android.diploma.common.data.dto.AreasDto
 import ru.practicum.android.diploma.common.data.dto.IndustriesDto
 import ru.practicum.android.diploma.common.data.dto.Resource
 import ru.practicum.android.diploma.common.data.dto.VacancyItemDto
+import ru.practicum.android.diploma.common.data.network.response.HHSearchResponse
 
 interface HHSearchRepository {
-    fun getVacancies(query: String, page: Int, perPage: Int): Flow<Resource<VacancyItemDto>>
-    fun getVacancy(id: String): Flow<Resource<VacancyItemDto>>
+    fun getVacancies(
+        query: String,
+        page: Int = 0,
+        perPage: Int = 20,
+        salary: Int? = null,
+        onlyWithSalary: Boolean = false
+    ): Flow<Resource<HHSearchResponse>>
+
+    fun getVacancy(id: Int): Flow<Resource<VacancyItemDto>>
     fun getAreas(): Flow<Resource<List<AreasDto>>>
     fun getIndustries(): Flow<Resource<List<IndustriesDto>?>>
 }
