04f668a22306ecb99aa65ec69335ec57e4ecb6f4
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-12-05 22:23:04 +0300

    Удаляет неиспользуемые импорты и папки

--- app/src/main/java/ru/practicum/android/diploma/vacancy/ui/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/ui/VacancyFragment.kt
@@ -1,21 +1,10 @@
 package ru.practicum.android.diploma.vacancy.ui
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.common.data.network.NetworkClient
-import ru.practicum.android.diploma.common.data.network.requests.AreasRequest
-import ru.practicum.android.diploma.common.data.network.requests.IndustriesRequest
-import ru.practicum.android.diploma.common.data.network.requests.SingleVacancyRequest
-import ru.practicum.android.diploma.common.data.network.requests.VacanciesSearchRequest
-import ru.practicum.android.diploma.common.data.network.response.SingleVacancyResponse
 import ru.practicum.android.diploma.common.ui.BaseFragment
-import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.databinding.FragmentVacancyBinding
-import ru.practicum.android.diploma.search.ui.viewmodels.SearchViewModel
 import javax.inject.Inject
 
 @AndroidEntryPoint
