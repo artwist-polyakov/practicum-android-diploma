e696423fb333554cb733416ad176cafafbfe5215
NikolayRyabcev <nikolas14@mail.ru>
2023-12-05 21:53:01 +0300

    1. Убран lateinit

--- app/src/main/java/ru/practicum/android/diploma/team/ui/fragments/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/team/ui/fragments/TeamFragment.kt
@@ -9,14 +9,19 @@ import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentTeamBinding
 
 class TeamFragment : Fragment() {
-    private lateinit var binding: FragmentTeamBinding
+    private var _binding: FragmentTeamBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentTeamBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentTeamBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
