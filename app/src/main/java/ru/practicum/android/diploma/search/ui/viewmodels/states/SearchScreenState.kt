894afdb1c3ef3200a12067b41d5e77207e801e6c
Aleksandr Poliakov <master@artwist.ru>
2023-12-06 19:40:12 +0300

    расширяет комментарий к классу состояний экрана

--- app/src/main/java/ru/practicum/android/diploma/search/ui/viewmodels/states/SearchScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/viewmodels/states/SearchScreenState.kt
@@ -2,8 +2,8 @@ package ru.practicum.android.diploma.search.ui.viewmodels.states
 
 sealed class SearchScreenState {
 
-    // todo заменить в двух местах Any,
-    //  когда будет понятно нужно ли хранить предыщуие сущности на экране
+    // todo заменить в двух местах Any, когда будет понятно
+    //  нужно ли хранить предыщуие сущности на экране и что отображаем
     data class Loading(val previousPage: Any) : SearchScreenState()
     data class Error(val error: ErrorsSearchScreenStates) : SearchScreenState()
     data class Result(val totalPages: Int, val currentPage: Int, val vacancies: Any) : SearchScreenState()
