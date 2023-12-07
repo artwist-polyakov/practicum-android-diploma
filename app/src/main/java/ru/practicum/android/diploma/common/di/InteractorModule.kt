6b8e5884f2c83722975d26b85c58de1e24cd9b07
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-12-07 21:33:17 +0300

    Резолвит конфликты

--- app/src/main/java/ru/practicum/android/diploma/common/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/di/InteractorModule.kt
@@ -8,6 +8,8 @@ import ru.practicum.android.diploma.search.data.network.HHSearchRepository
 import ru.practicum.android.diploma.search.domain.api.SearchInteractor
 import ru.practicum.android.diploma.search.domain.api.SearchResultConverter
 import ru.practicum.android.diploma.search.domain.impl.SearchInteractorImpl
+import ru.practicum.android.diploma.vacancy.domain.impl.VacancyInteractor
+import ru.practicum.android.diploma.vacancy.domain.impl.VacancyInteractorImpl
 
 @Module
 @InstallIn(ViewModelComponent::class)
