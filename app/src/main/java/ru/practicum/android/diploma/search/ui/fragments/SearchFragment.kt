21cd31f1e9f2e86843c9a8a6b82d55292f1efa0a
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-12-07 13:36:47 +0300

    Форматирует код Удаляяет лишние комменты

--- app/src/main/java/ru/practicum/android/diploma/search/ui/fragments/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/fragments/SearchFragment.kt
@@ -1,8 +1,6 @@
 package ru.practicum.android.diploma.search.ui.fragments
 
-import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,9 +13,6 @@ import ru.practicum.android.diploma.search.ui.viewmodels.states.SearchScreenStat
 @AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(FragmentSearchBinding::inflate) {
     override val viewModel: SearchViewModel by viewModels()
-
-//    @Inject
-//    lateinit var interactor: SearchInteractor
 
     override fun initViews() {
         viewModel.getVacancies("android")
@@ -49,43 +44,5 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(Frag
                 Log.i("SearchFragmentEmptyMyLog", "Empty state")
             }
         }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // todo  удалить после отладки
-//        viewLifecycleOwner.lifecycleScope.launch {
-//            interactor.searchVacancies(text = "android")
-//                .collect {
-//                    it.data?.vacancies?.forEach { vacancy ->
-//                        Log.i("SearchFragmentMyLog", "vacancy: ${vacancy.title}")
-//                    }
-//                }
-//            repo.getAreas()
-//                .collect {
-//                    it.data?.areas?.forEach { area ->
-//                        Log.d("SearchFragment", "area: ${area.name}")
-//                    }
-//                }
-//            repo.getIndustries()
-//                .collect {
-//                    it.data?.industries?.forEach { industry ->
-//                        Log.d("SearchFragment", "industry: ${industry.name}")
-//                    }
-//                }
-//            repo.getVacancy(90190128)
-//                .collect {
-//                    it.data?.vacancy?.let { vacancy ->
-//                        Log.d("SearchFragment", "vacancy: ${vacancy.description}")
-//                    }
-//                }
-//            repo.getSimilarVacancies(90190128)
-//                .collect {
-//                    it.data?.items?.forEach { vacancy ->
-//                        Log.d("SearchFragment", "vacancy similar: ${vacancy.name}")
-//                    }
-//                }
-//    }
     }
 }
