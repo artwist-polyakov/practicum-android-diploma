8f4307e0d810a4819f97c808acffb05f917cfb7a
Anton Kochekov <95627984+avkochekov@users.noreply.github.com>
2023-12-09 09:05:04 +0600

    Update SearchScreenState.kt

--- app/src/main/java/ru/practicum/android/diploma/search/ui/viewmodels/states/SearchScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/viewmodels/states/SearchScreenState.kt
@@ -1,10 +1,12 @@
 package ru.practicum.android.diploma.search.ui.viewmodels.states
 
+import ru.practicum.android.diploma.search.domain.models.VacancyGeneral
+
 sealed class SearchScreenState {
 
     // todo заменить в двух местах Any, когда будет понятно
     //  нужно ли хранить предыщуие сущности на экране и что отображаем
     data object Loading : SearchScreenState()
     data class Error(val error: ErrorsSearchScreenStates) : SearchScreenState()
-    data class Content(val totalPages: Int, val currentPage: Int, val vacancies: Any) : SearchScreenState()
+    data class Content(val totalPages: Int, val currentPage: Int, val vacancies: List<VacancyGeneral>) : SearchScreenState()
 }
