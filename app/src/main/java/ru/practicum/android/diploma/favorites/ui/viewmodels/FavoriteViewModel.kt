12630d942b2a11d0de82d103f38dbe0128a3ffd7
Aleksandr Poliakov <master@artwist.ru>
2023-12-15 11:01:50 +0300

    создаёт метод получения исходного состояния вьюмодели

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/viewmodels/FavoriteViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/viewmodels/FavoriteViewModel.kt
@@ -1,7 +1,12 @@
 package ru.practicum.android.diploma.favorites.ui.viewmodels
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.common.ui.BaseViewModel
 import ru.practicum.android.diploma.favorites.domain.api.FavoritesDBInteractor
 import ru.practicum.android.diploma.favorites.ui.viewmodels.states.FavoritesScreenState
@@ -13,12 +18,49 @@ class FavoriteViewModel @Inject constructor(
     private val interactor: FavoritesDBInteractor
 ) : BaseViewModel() {
     private var vacancies: MutableList<VacancyGeneral> = mutableListOf()
-    private var showSnackBar: Boolean = false
 
     private var totalPages: Int = 0
-    private var isLastUpdatePage = false
+    private var currentPage: Int = 0
 
     private var _state = MutableStateFlow<FavoritesScreenState>(FavoritesScreenState.Empty())
         get() = _state
+
+    init {
+        Log.d("FavoritesViewModel", "init")
+        checkState()
+    }
+
+    private fun checkState() {
+        viewModelScope.launch {
+            interactor.getFavoritesVacancies(page = currentPage)
+                .catch {
+                    _state.value = FavoritesScreenState.Error()
+                }
+                .collect {
+                    if (it.vacancies.isEmpty()) {
+                        totalPages = 0
+                        currentPage = 0
+                        vacancies.clear()
+                        _state.value = FavoritesScreenState.Empty()
+                        Log.d("FavoritesViewModel", "Favorites is empty")
+                    } else {
+                        totalPages = it.totalPages
+                        currentPage = it.currentPage
+                        vacancies.addAll(it.vacancies)
+                        _state.value = FavoritesScreenState.Content(
+                            totalPages = totalPages,
+                            currentPage = currentPage,
+                            totalVacancies = it.vacanciesFound,
+                            vacancies = vacancies
+                        )
+                        Log.d("FavoritesViewModel", "Favorites is not empty")
+                        Log.d("FavoritesViewModel", "Total: ${it.vacanciesFound}")
+                        Log.d("FavoritesViewModel", "Vacancies: $vacancies")
+                    }
+                }
+
+        }
+    }
+
 
 }
