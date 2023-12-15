b20e255ee236701b41d98241e258a43aae5256e4
Aleksandr Poliakov <master@artwist.ru>
2023-12-15 10:48:24 +0300

    добавляет состояние экрана для вм вакансий

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/viewmodels/FavoriteViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/viewmodels/FavoriteViewModel.kt
@@ -1,8 +1,24 @@
 package ru.practicum.android.diploma.favorites.ui.viewmodels
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import ru.practicum.android.diploma.common.ui.BaseViewModel
+import ru.practicum.android.diploma.favorites.domain.api.FavoritesDBInteractor
+import ru.practicum.android.diploma.favorites.ui.viewmodels.states.FavoritesScreenState
+import ru.practicum.android.diploma.search.domain.models.VacancyGeneral
 import javax.inject.Inject
 
 @HiltViewModel
-class FavoriteViewModel @Inject constructor() : BaseViewModel()
+class FavoriteViewModel @Inject constructor(
+    private val interactor: FavoritesDBInteractor
+) : BaseViewModel() {
+    private var vacancies: MutableList<VacancyGeneral> = mutableListOf()
+    private var showSnackBar: Boolean = false
+
+    private var totalPages: Int = 0
+    private var isLastUpdatePage = false
+
+    private var _state = MutableStateFlow<FavoritesScreenState>(FavoritesScreenState.Empty())
+        get() = _state
+
+}
