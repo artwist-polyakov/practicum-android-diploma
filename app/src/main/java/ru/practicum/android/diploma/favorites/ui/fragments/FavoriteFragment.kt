780b78f7eace7f500990d8cbaa35c6761ffc1ada
Nikolay <nikolas14@mail.ru>
2023-12-16 22:02:40 +0300

    1. Делает первую отрисовку

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/fragments/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/fragments/FavoriteFragment.kt
@@ -1,25 +1,54 @@
 package ru.practicum.android.diploma.favorites.ui.fragments
 
+import android.os.Bundle
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.common.ui.BaseFragment
+import ru.practicum.android.diploma.common.utils.debounce
 import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
 import ru.practicum.android.diploma.favorites.ui.viewmodels.FavoriteViewModel
 import ru.practicum.android.diploma.favorites.ui.viewmodels.states.FavoritesScreenState
+import ru.practicum.android.diploma.search.domain.models.VacancyGeneral
+import ru.practicum.android.diploma.search.ui.fragments.SearchFragment
+import ru.practicum.android.diploma.search.ui.fragments.VacancyAdapter
+import ru.practicum.android.diploma.vacancy.ui.VacancyFragment
 
 @AndroidEntryPoint
 class FavoriteFragment : BaseFragment<FragmentFavoriteBinding, FavoriteViewModel>(FragmentFavoriteBinding::inflate) {
     override val viewModel by viewModels<FavoriteViewModel>()
+    private var onVacancyClickDebounce: ((VacancyGeneral) -> Unit)? = null
+    private val vacancyListAdapter = VacancyAdapter { data ->
+        onVacancyClickDebounce?.invoke(data)
+    }
 
     override fun initViews() {
-        // Блок для инициализации ui
-
+        binding.favoritesList.root.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = vacancyListAdapter
+        }
     }
 
     override fun subscribe() {
-        // Блок для подписок (клики, viewModel)
+        onVacancyClickDebounce = debounce(
+            CLICK_DEBOUNCE_DELAY,
+            viewLifecycleOwner.lifecycleScope,
+            false,
+            false
+        ) { data ->
+            val bundle = Bundle().apply {
+                putInt(VacancyFragment.ARG_ID, data.id)
+            }
+            findNavController().navigate(
+                R.id.action_searchFragment_to_vacancyFragment,
+                bundle
+            )
+        }
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.state.collect { state ->
                 render(state)
@@ -32,15 +61,22 @@ class FavoriteFragment : BaseFragment<FragmentFavoriteBinding, FavoriteViewModel
             is FavoritesScreenState.Empty -> {
                 // Блок для отображения пустого экрана
             }
+
             is FavoritesScreenState.Content -> {
                 // Блок для отображения контента
             }
+
             is FavoritesScreenState.Error -> {
                 // Блок для отображения ошибки
             }
+
             is FavoritesScreenState.Loading -> {
                 // Блок для отображения загрузки
             }
         }
+    }
+
+    companion object {
+        private const val CLICK_DEBOUNCE_DELAY = 500L
     }
 }
