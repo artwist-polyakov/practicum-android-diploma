df9ff4eb610c8941e06e56590f1694b9046cd422
NikolayRyabcev <nikolas14@mail.ru>
2023-12-17 12:33:36 +0300

    1. Исправлены ошибки навигации

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/fragments/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/fragments/FavoriteFragment.kt
@@ -2,6 +2,7 @@ package ru.practicum.android.diploma.favorites.ui.fragments
 
 import android.view.View.VISIBLE
 import android.os.Bundle
+import android.view.View
 import android.view.View.GONE
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
