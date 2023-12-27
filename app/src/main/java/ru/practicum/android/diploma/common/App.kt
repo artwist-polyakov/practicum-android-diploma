4322846e80ab67969510274aa21ac7277dfc3d41
Aleksandr Poliakov <master@artwist.ru>
2023-12-27 19:12:24 +0300

    чинит локаль

--- app/src/main/java/ru/practicum/android/diploma/common/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/App.kt
@@ -2,6 +2,16 @@ package ru.practicum.android.diploma.common
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import java.util.Locale
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val locale = Locale("ru")
+        Locale.setDefault(locale)
+        baseContext.resources.configuration.setLocale(locale)
+    }
+
+}
